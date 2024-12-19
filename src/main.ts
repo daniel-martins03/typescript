c9b1b204a9704ee12e200c5d22e65f880dd5746a
Daniel <daniel003tm@gmail.com>
2024-12-18 21:34:06 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação que recebe dois números e retorna o resultado da multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+  }
+
+// Função de saudação que recebe um nome e retorna a mensagem de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+  }
+  
+  // Exemplos de uso:
+  console.log(multiplicar(2, 3)); // 6
+  console.log(saudacao("Maria")); // "Olá Maria"
